1be0953bb82067a0923177b581801873a5c8b8d8
Xin <431918700@qq.com>
2019-05-09 18:11:39 +0800

    add

---
+++ src/main/java/com/xin/admin/XinAdminApplication.java
@@ -0,0 +1,19 @@
+package com.xin.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 类功能描述: SpringBootApplication启动类
+ *
+ * @author Eternal
+ * @date 2019/5/9 17:23
+ */
+@SpringBootApplication
+public class XinAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XinAdminApplication.class, args);
+	}
+
+}
